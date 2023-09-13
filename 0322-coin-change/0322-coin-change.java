eace5239aa2e5c554bcbb6f662ce71daa86dee54
adityaraj9900 <102577534+adityaraj9900@users.noreply.github.com>
2023-09-14 02:26:07 +0530

    Time: 18 ms (48%), Space: 43.7 MB (35.57%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,22 +2,22 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
         int[][] dp=new int[n+1][amount+1];
-        for (int i=0;i<=n;i++){
+        for (int i=0;i<n+1;i++){
             dp[i][0]=0;
         }
-        for (int j=1;j<=amount;j++){
+        for (int j=0;j<amount+1;j++){
             dp[0][j]=amount+1;
         }
-        for (int i=1;i<=n;i++) {
-            for (int j=1;j<=amount;j++) {
+        for (int i=1;i<n+1;i++) {
+            for (int j=1;j<amount+1;j++) {
                 if (coins[i-1]<=j) {
-                    dp[i][j]=Math.min(dp[i-1][j],dp[i][j-coins[i-1]]+1);
+                    dp[i][j]=Math.min(dp[i][j-coins[i-1]]+1,dp[i-1][j]);
                 }else{
                     dp[i][j]=dp[i-1][j];
                 }
             }
         }
-        int result=0;
+        int result;
         if (dp[n][amount]>amount) {
             result=-1;
         }else{
