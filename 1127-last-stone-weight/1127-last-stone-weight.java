7fe2b97b894fb163dcae6a2c77335afd9a12d705
adityaraj9900 <102577534+adityaraj9900@users.noreply.github.com>
2023-09-16 00:11:47 +0530

    Time: 1 ms (99%), Space: 39.6 MB (93.78%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.java
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=n-1;i>0;i--){
             int a=stones[i];
             int b=stones[i-1];
-            stones[i-1]=Math.abs(a-b);
+            stones[i-1]=a-b;
             Arrays.sort(stones,0,i);
         }
         return stones[0];
