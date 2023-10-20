b4bfb431085ad3a193bb64347a03706061758d74
adityaraj9900 <102577534+adityaraj9900@users.noreply.github.com>
2023-10-21 01:29:15 +0530

    Time: 2 ms (89.45%), Space: 46.3 MB (17.34%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void reorderList(ListNode head) {
+        //find mid
+    	ListNode slow=head;
+    	ListNode fast=head.next;
+    	while(fast!=null&&fast.next!=null) {
+    		slow=slow.next;
+    		fast=fast.next.next;
+    	}
+    	ListNode mid=slow;
+    	//Reverse 2nd half
+    	ListNode curr=mid.next;
+    	mid.next=null;
+    	ListNode prev=null;
+    	ListNode next;
+    	while(curr!=null) {
+    		next=curr.next;
+    		curr.next=prev;
+    		prev=curr;
+    		curr=next;
+    	}
+    	ListNode left=head;
+    	ListNode right=prev;
+    	ListNode nextL,nextR;
+    	//alt merge-zig-zag merge
+    	while(left!=null&&right!=null) {
+    		nextL=left.next;
+    		left.next=right;
+    		nextR=right.next;
+    		right.next=nextL;
+    		left=nextL;
+    		right=nextR;
+    	}
+    }
+}
