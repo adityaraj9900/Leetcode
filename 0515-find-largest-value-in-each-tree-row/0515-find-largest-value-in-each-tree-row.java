16dfbba2da489d09474afbde1f020bfe07059c17
adityaraj9900 <102577534+adityaraj9900@users.noreply.github.com>
2023-10-24 21:25:32 +0530

    Time: 2 ms (82.84%), Space: 43.7 MB (89.96%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -1,26 +1,26 @@
 class Solution {
-    public List<Integer> largestValues(TreeNode root) {
-        List<Integer> result=new ArrayList<>();
+    public List<Integer> largestValues(TreeNode root) { //use bfs queue use hoga    
+        List<Integer> result=new ArrayList<>();  //reuslt heighest in a row ko store karega
         if(root==null){
             return result;
         }
-        Queue<TreeNode>q=new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            int size=q.size();
-            int max=Integer.MIN_VALUE;
-            for(int i=0;i<size;i++){
-                TreeNode node=q.poll();
-                max=Math.max(max,node.val);
-                if(node.left!=null){
+        Queue<TreeNode>q=new LinkedList<>();  //create a queue
+        q.offer(root);   //add first root  
+        while(!q.isEmpty()){ //when my queue is not empty then go to next step
+            int size=q.size(); //intialize the size of queue
+            int max=Integer.MIN_VALUE;  //intialize max
+            for(int i=0;i<size;i++){   //traverse the loop
+                TreeNode node=q.poll();  
+                max=Math.max(max,node.val);  //find the max 
+                if(node.left!=null){  //left child
                     q.offer(node.left);
                 }
-                if(node.right!=null){
+                if(node.right!=null){   //right child
                     q.offer(node.right);
                 }
             }
-            result.add(max);
+            result.add(max); //add the max value in the result arraylist
         }
-        return result;
+        return result; 
     }
 }
